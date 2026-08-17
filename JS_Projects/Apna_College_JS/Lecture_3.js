c0a039bc2e55860f8d15defd692fee662ae7160c
Fahadkhan56 <khangfahad56@gmail.com>
2026-08-17 17:30:12 +0500

    Apna College Lecture 3 Practice work complete

--- JS_Projects/Apna_College_JS/Lecture_3.js
+++ JS_Projects/Apna_College_JS/Lecture_3.js
@@ -3,12 +3,17 @@
 //         console.log(i);
 //     };
 // }
-let is_running = true;
-let random_num = Math.floor(Math.random() * 10);
-while (is_running) {
-    let user_num = prompt("Guess a Number Blw 1 & 10");
-    if(user_num == random_num){
-        console.log("You Win",user_num);
-        is_running = false
-    }else{console.log("Try again");};
-}
+// let is_running = true;
+// let random_num = Math.floor(Math.random() * 10);
+// while (is_running) {
+//     let user_num = prompt("Guess a Number Blw 1 & 10");
+//     if(user_num == random_num){
+//         console.log("You Win",user_num);
+//         is_running = false
+//     }else{console.log("Try again");};
+// }
+
+// const { use } = require("react
+
+let user_name = prompt("Write you Name");
+console.log(`@${user_name.concat(user_name.length)}`);
