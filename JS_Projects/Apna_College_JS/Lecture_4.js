11874c16777861cd7b67df5652030e09e23d071a
Fahadkhan56 <khangfahad56@gmail.com>
2026-08-26 11:47:02 +0500

    Apna College Lecture 4 and 5 of JS

---
+++ JS_Projects/Apna_College_JS/Lecture_4.js
@@ -0,0 +1,22 @@
+// let marks = [85,97,44,37,76,60];
+// let total = 0
+// for (let ele of marks) {
+//     total += ele;
+// }
+// let ave = total/marks.length;
+// console.log(ave);
+
+// let prices = [250,645,300,900,50];
+// for(let i = 0; i < prices.length; i++){
+//     prices[i] = prices[i] - (prices[i]/10)
+// };
+// console.log(prices);
+
+// let companies = ["Bloombery","Microsoft","Uber","Google","Netflix"];
+// console.log(companies);
+// console.log(companies.shift());
+// console.log(companies.splice(1,1,"Ola"));
+// console.log(companies);
+// companies.push("Amazom")
+// console.log(companies);
+
---
+++ JS_Projects/Apna_College_JS/Lecture_5.js
@@ -0,0 +1,33 @@
+// const arrow_mutl = (x,y)=>{
+//     console.log(x*y);
+// };
+
+// function check_vowels(str){
+//     let total = 0;
+//     for (let i = 0; i < str.length; i++) {
+//         if(str[i] == "a" || str[i] == "e" || str[i] == "i" || str[i] == "o" || str[i] == "u"){
+//             total++
+//         };
+//     };
+//     console.log(total);
+// };
+// const check_vowels = (str) => {
+//   let total = 0;
+//   for (let i = 0; i < str.length; i++) {
+//     if (
+//       str[i] == "a" ||
+//       str[i] == "e" ||
+//       str[i] == "i" ||
+//       str[i] == "o" ||
+//       str[i] == "u"
+//     ) {
+//       total++;
+//     }
+//   }
+//   console.log(total);
+// };
+
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+num.forEach((val) => {
+  console.log(val * val);
+});
