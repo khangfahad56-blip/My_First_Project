01a2831e02221427fbff4a88839846e428e603b1
Fahadkhan56 <khangfahad56@gmail.com>
2026-08-28 12:24:25 +0500

    Tic Tac Toe Complete

--- JS_Projects/Tic_Tac_Toe_JS/app.js
+++ JS_Projects/Tic_Tac_Toe_JS/app.js
@@ -1,51 +1,69 @@
-// tick &#10004; and cross &#10008;
-
-// Box variables
-
 let container = document.querySelector(".container");
-
-let box1 = document.querySelector("#box1");
-let box2 = document.querySelector("#box2");
-let box3 = document.querySelector("#box3");
-let box4 = document.querySelector("#box4");
-let box5 = document.querySelector("#box5");
-let box6 = document.querySelector("#box6");
-let box7 = document.querySelector("#box7");
-let box8 = document.querySelector("#box8");
-let box9 = document.querySelector("#box9");
-
-// True is Player 1 and False is Player 2
-
 let turn = 1;
-function put_mark(event){
-    let box_num = event.target
-    if(turn === 1){
-        box_num.innerHTML = "&#10004";
-        box_num.classList.add("tick");
-        box_num.classList.remove("cross")
-        turn = 0;
-    }else{
-        box_num.innerHTML = "&#10008";
-        box_num.classList.add("cross");
-        box_num.classList.remove("tick")
-        turn = 1;
-    };
-    
-};
+let boxes = ["", "", "", "", "", "", "", "", ""];
+let is_running = false;
+function put_mark(event) {
+  if (is_running) {
+    return;
+  }
+  let box_num = event.target;
+  let box_id = event.currentTarget.id;
+  if (box_num.textContent.trim() === "") {
+    if (turn === 1) {
+      box_num.innerHTML = "O";
+      box_num.classList.add("circle");
+      box_num.classList.remove("cross");
+      turn = 0;
+      boxes[box_id] = "O";
+    } else {
+      box_num.innerHTML = "X";
+      box_num.classList.add("cross");
+      box_num.classList.remove("circle");
+      turn = 1;
+      boxes[box_id] = "X";
+    }
+  } else {
+    console.log("You Click again");
+  }
+  if (
+    (boxes[0] === "O" && boxes[1] === "O" && boxes[2] === "O") ||
+    (boxes[3] === "O" && boxes[4] === "O" && boxes[5] === "O") ||
+    (boxes[6] === "O" && boxes[7] === "O" && boxes[8] === "O") ||
+    (boxes[0] === "O" && boxes[3] === "O" && boxes[6] === "O") ||
+    (boxes[1] === "O" && boxes[4] === "O" && boxes[7] === "O") ||
+    (boxes[2] === "O" && boxes[5] === "O" && boxes[8] === "O") ||
+    (boxes[0] === "O" && boxes[4] === "O" && boxes[8] === "O") ||
+    (boxes[2] === "O" && boxes[4] === "O" && boxes[6] === "O")
+  ) {
+    console.log("Circle Win");
+    is_running = true;
+  } else if (
+    (boxes[0] === "X" && boxes[1] === "X" && boxes[2] === "X") ||
+    (boxes[3] === "X" && boxes[4] === "X" && boxes[5] === "X") ||
+    (boxes[6] === "X" && boxes[7] === "X" && boxes[8] === "X") ||
+    (boxes[0] === "X" && boxes[3] === "X" && boxes[6] === "X") ||
+    (boxes[1] === "X" && boxes[4] === "X" && boxes[7] === "X") ||
+    (boxes[2] === "X" && boxes[5] === "X" && boxes[8] === "X") ||
+    (boxes[0] === "X" && boxes[4] === "X" && boxes[8] === "X") ||
+    (boxes[2] === "X" && boxes[4] === "X" && boxes[6] === "X")
+  ) {
+    console.log("Cross Win");
+    is_running = true;
+  }
 
-// box1.addEventListener("click", put_mark);
-// box2.addEventListener("click", put_mark);
-// box3.addEventListener("click", put_mark);
-// box4.addEventListener("click", put_mark);
-// box5.addEventListener("click", put_mark);
-// box6.addEventListener("click", put_mark);
-// box7.addEventListener("click", put_mark);
-// box8.addEventListener("click", put_mark);
-// box9.addEventListener("click", put_mark);
+  //   let winning_statement = document.createElement("p");
+  //   winning_statement.innerText()
+}
 
 for (let box of container.children) {
-    if(box.innerHTML !== "&#10008" || box.innerHTML !== "&#10004"){
-        console.dir(box.innerHTML);
-        box.addEventListener("click", put_mark);
-    };
+  box.addEventListener("click", put_mark);
 }
+
+// 0-1-2
+// 3-4-5
+// 6-7-8
+// 0-3-6
+// 1-4-7
+// 2-5-8
+// 0-4-8
+// 2-4-6
